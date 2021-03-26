07439675ef62d35501ff6f51d03b8a52b6445d59
Keven <hello@keven.fr>
2021-03-26 12:56:03 +0100

    Add addRoutes() to AltoDispatcher

--- src/AltoDispatcher.php
+++ src/AltoDispatcher.php
@@ -3,6 +3,7 @@
 namespace Interop\Routing\Alto;
 
 use Interop\Routing\DispatcherInterface;
+use Interop\Routing\Route\RouteCollection;
 use Psr\Http\Message\ServerRequestInterface;
 use AltoRouter;
 
@@ -13,6 +14,15 @@ final class AltoDispatcher implements DispatcherInterface
     public function __construct(AltoRouter $router)
     {
         $this->router = $router;
+    }
+
+    public function addRoutes(RouteCollection $routes): self
+    {
+        foreach ($routes as $route) {
+            $this->router->map(implode('|', $route->getMethods()), $route->getPath(), $route->getHandler(), $route->getName());
+        }
+
+        return $this;
     }
 
     public function dispatch(ServerRequestInterface $request): callable
